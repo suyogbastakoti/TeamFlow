600aa25a4171d06d8eea0e81ab30c8d702037715
Suyog <suyog017@gmail.com>
2026-09-09 16:05:18 +0545

    Prepare backend for Render

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ mongoose
     .then(() => {
         console.log("Connected to MongoDB");
 
-        app.listen(process.env.PORT, () => {
+        app.listen(process.env.PORT || PORT, () => {
             console.log("Server running on port:", process.env.PORT);
         });
     })
